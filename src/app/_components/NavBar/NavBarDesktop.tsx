b00a92dedc8c1e4e838cd8cad91bae492a4fd6c5
yymin1022 <yymin1022@gmail.com>
2024-01-30 21:51:43 +0900

    Update NavBarDesktop.tsx : Cleaned UP

--- src/app/_components/NavBar/NavBarDesktop.tsx
+++ src/app/_components/NavBar/NavBarDesktop.tsx
@@ -2,13 +2,13 @@ import NavButton from "@/app/_components/NavBar/NavButton";
 
 const NavBarDesktop = () => {
     return (
-        <nav>
-            <div className="flex flex-row h-30 mt-[15px]">
+        <nav className="w-full">
+            <div className="flex flex-row justify-between w-full h-30 mt-[15px]">
                 <div>
                     <img className="h-30 ml-[80px]" src="/HeaderLogo.svg" alt="logo"/>
                 </div>
                 <div className="w-full h-30 flex justify-end items-center">
-                    <NavButton link="/activities">Activites</NavButton>
+                    <NavButton link="/activities">Activities</NavButton>
                     <NavButton link="/members">Members</NavButton>
                     <NavButton link="/things">Things</NavButton>
                 </div>
