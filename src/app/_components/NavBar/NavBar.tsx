e1f20c8ace0c5fdbb14b74666128260319dea305
yymin1022 <yymin1022@gmail.com>
2024-01-30 21:48:32 +0900

    Update NavBar.tsx : Cleaned uP

--- src/app/_components/NavBar/NavBar.tsx
+++ src/app/_components/NavBar/NavBar.tsx
@@ -1,9 +1,8 @@
 import NavBarDesktop from "@/app/_components/NavBar/NavBarDesktop";
 
-
 export const NavBar = () => {
     return(
-        <div className="flex-row w-full fixed top-0">
+        <div className="flex flex-row w-full fixed top-0">
             <NavBarDesktop/>
         </div>
     )
