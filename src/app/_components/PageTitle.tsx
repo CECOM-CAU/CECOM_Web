9f1e794c35baf453b35eb53421d4d5260014f345
sereene <salad1223@naver.com>
2024-01-30 22:15:10 +0900

    Added things page

---
+++ src/app/_components/PageTitle.tsx
@@ -0,0 +1,9 @@
+
+
+const ThingsTitle = () => {
+    return (
+        <p className={'text-[160px] font-gmarket'}>Things</p>
+    );
+}
+
+export default ThingsTitle;
---
+++ src/app/things/page.tsx
@@ -0,0 +1,13 @@
+import PageTitle from "@/app/_components/PageTitle";
+import ThingsTitle from "@/app/_components/PageTitle";
+
+
+const ThingsPage = () => {
+    return (
+<div className={'mt-[95px]'}>
+    <ThingsTitle></ThingsTitle>
+</div>
+    );
+}
+
+export default ThingsPage;
