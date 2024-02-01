14bb8d1a75ed2f9c6c6b629549c93ae2e4a1db64
Lim-shweety <smile70200@naver.com>
2024-02-01 21:02:47 +0900

    Update NavButton.tsx : fix reRendering problem

--- src/app/_components/NavBar/NavButton.tsx
+++ src/app/_components/NavBar/NavButton.tsx
@@ -4,23 +4,18 @@ import Link from "next/link";
 interface Props {
     children: React.ReactNode;
     link: string;
-    click: true | false;
+    isClick: boolean;
 }
 
-const NavButton = ({children, link, click}: Props) => {
-    if (click) {
-        return (
-            <Link href={`${link}`}>
-                <p className="px-4 py-2 rounded-3xl ml-[20px] mr-[20px] font-gmarket-m bg-primary-lightgray30 ">{children}</p>
-            </Link>
-        )
-    } else {
-        return (
-            <Link href={`${link}`}>
-                <p className="px-4 py-2 mr-[20px] ml-[20px]">{children}</p>
-            </Link>
-        )
-    }
+const NavButton = ({children, link, isClick}: Props) => {
+    const NAV_BTN_STYLE_CLICKED = "px-4 py-2 rounded-3xl ml-[20px] mr-[20px] font-gmarket-m bg-primary-lightgray30 "
+    const NAV_BTN_STYLE_NORMAL = "px-4 py-2 mr-[20px] ml-[20px]"
+
+    return (
+        <Link href={`${link}`}>
+            <p className={isClick?NAV_BTN_STYLE_CLICKED:NAV_BTN_STYLE_NORMAL}>{children}</p>
+        </Link>
+    )
 }
 
 export default NavButton;
