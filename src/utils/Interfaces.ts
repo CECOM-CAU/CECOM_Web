4bda0dfd9ee839bd883c26443414c18465a8aa39
yymin1022 <yymin1022@gmail.com>
2024-02-04 17:17:01 +0900

    Update Interface : Added Activity related Interfaces

--- src/utils/Interfaces.ts
+++ src/utils/Interfaces.ts
@@ -1,3 +1,39 @@
+interface Activity {
+    count: number
+    list: Array<ActivityItem>
+}
+
+interface ActivityItem {
+    member: Array<string>
+    mento: string
+    tag: Array<string>
+    thumbnail: string
+    title: string
+}
+
+interface ActivityContent {
+    content: string
+    role: Array<ActivityContentRoleItem>
+}
+
+interface ActivityContentRoleItem {
+    id: string
+    member: Array<ActivityContentRoleItemMember>
+    photo: ActivityContentRoleItemPhoto
+    title: string
+}
+
+interface ActivityContentRoleItemMember {
+    icon: string
+    name: string
+    url: string
+}
+
+interface ActivityContentRoleItemPhoto {
+    count: number,
+    data: Array<string>
+}
+
 interface Admin {
     list: Array<AdminItem>
     year: number
