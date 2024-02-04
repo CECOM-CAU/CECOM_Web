f345ff7202bd42554cb5b2596d35dffea4d1eda2
yymin1022 <yymin1022@gmail.com>
2024-02-04 17:58:11 +0900

    Update Interfaces : Added API Related Interface

--- src/utils/Interfaces.ts
+++ src/utils/Interfaces.ts
@@ -1,3 +1,9 @@
+export interface API_RESULT {
+    RESULT_CODE: number
+    RESULT_MSG: string
+    RESULT_DATA: Activity | ActivityContent | Admin | Member | Thing | undefined
+}
+
 export interface Activity {
     count: number
     data: Array<ActivityItem>
