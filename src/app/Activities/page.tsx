ca57c83a4d4426b497ca49c38356fb0d69b088d7
Lim-shweety <smile70200@naver.com>
2024-02-05 03:04:22 +0900

    Update NavBar : resolve git conflict

--- src/app/Activities/page.tsx
+++ src/app/Activities/page.tsx
@@ -1,37 +1,49 @@
 "use client";
 import PageTitle from "@/app/_components/PageTitle";
-import MenuBTN from "@/app/activities/_components/MenuBTN";
 import {useState} from "react";
-import ActivityCard from "@/app/activities/_components/ActivityCard";
 import {ActivityListData} from "@/utils/DummyData";
-import {Activity, ActivityItem} from "@/utils/Interfaces";
-import MainImg from "/Thingsphoto.jpg";
+import {ActivityItem} from "@/utils/Interfaces";
+import MenuBTN from "@/app/activities/_components/MenuBTN";
+import ActivityCard from "@/app/activities/_components/ActivityCard";
+
 
 const ActivitiesPage = () => {
     const [all, setAll] = useState<boolean>(true);
     const [project, setProject] = useState<boolean>(false);
     const [mentoring, setMentoring] = useState<boolean>(false);
     const [study, setStudy] = useState<boolean>(false);
+    const handleMenuBtn = (isAll: boolean, isProject: boolean, isMentoring: boolean, isStudy: boolean) => {
+        setAll(isAll);
+        setProject(isProject);
+        setMentoring(isMentoring);
+        setStudy(isStudy);
+
+    }
 
     return (
         <div className="w-full flex flex-col">
             <PageTitle>Activities</PageTitle>
             <nav className="w-full flex flex-row justify-center ">
-                <MenuBTN isClicked={all}><p>ALL</p></MenuBTN>
-                <MenuBTN isClicked={project}><p>Project</p></MenuBTN>
-                <MenuBTN isClicked={mentoring}><p>Mentoring</p></MenuBTN>
-                <MenuBTN isClicked={study}><p>Study</p></MenuBTN>
+                <div onClick={() => handleMenuBtn(true, false, false, false)}>
+                    <MenuBTN isClicked={all}>ALL</MenuBTN>
+                </div>
+                <div onClick={() => handleMenuBtn(false, true, false, false)}>
+                    <MenuBTN isClicked={project}>Project</MenuBTN>
+                </div>
+                <div onClick={() => handleMenuBtn(false, false, true, false)}>
+                    <MenuBTN isClicked={mentoring}>Mentoring</MenuBTN>
+                </div>
+                <div onClick={() => handleMenuBtn(false, false, false, true)}>
+                    <MenuBTN isClicked={study}>Study</MenuBTN>
+                </div>
             </nav>
             <div className="flex flex-col items-center">
-
                 <div
-                    className="grid container w-[75%] grid-cols-4 grid-flow-row gap-[10px] justify-items-center">
-
+                    className="grid container max-w-[1300px] grid-cols-4 grid-flow-row gap-[10px] justify-items-center">
                     {ActivityListData.data.map(({member, mento, tag, thumbnail, title}: ActivityItem) => (
                         <ActivityCard member={member} mento={mento} tag={tag} thumbnail={"/Activities.jpg"}
-                                      title={title}/>
-
-                    ))}
+                                      title={title}/>))
+                    }
                 </div>
             </div>
         </div>
--- src/app/_components/NavBar/NavButton.tsx
+++ src/app/_components/NavBar/NavButton.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, {useEffect, useState} from "react";
 import Link from "next/link";
 
 interface Props {
@@ -10,7 +11,10 @@ interface Props {
 const NavButton = ({children, link, isClick}: Props) => {
     const NAV_BTN_STYLE_CLICKED = "px-4 py-2 rounded-3xl ml-[20px] mr-[20px] font-gmarket-m bg-primary-lightgray30 "
     const NAV_BTN_STYLE_NORMAL = "px-4 py-2 mr-[20px] ml-[20px]"
-
+    const [mounted, setMounted] = useState<boolean>(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
     return (
         <Link href={link}>
             <div className={isClick ? NAV_BTN_STYLE_CLICKED : NAV_BTN_STYLE_NORMAL}>{children}</div>
