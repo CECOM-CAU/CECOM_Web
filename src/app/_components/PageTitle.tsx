c29e0f1d6a21d5d80a69e9ad367396405d21a6d3
Lim-shweety <smile70200@naver.com>
2024-02-04 16:55:57 +0900

    Update PageTitle.tsx : child component flex remove

--- src/app/_components/PageTitle.tsx
+++ src/app/_components/PageTitle.tsx
@@ -6,8 +6,8 @@ interface Props{
 const PageTitle = ({children}:Props) => {
     return (
         <div className="flex flex-row">
-            <p className="flex text-[130px] mt-[10px] ml-[120px]">{children}</p>
-            <img className="h-[60px] flex ml-[-15px] mt-[10px]" src="/Titlelogo.png" alt="Titlelogo"/>
+            <p className="text-[130px] mt-[10px] ml-[120px]">{children}</p>
+            <img className="h-[60px] ml-[-15px] mt-[10px]" src="/Titlelogo.png" alt="Titlelogo"/>
         </div>
     );
 }
