05fa328f5290a9b34f5c07f06c861d2b2bf52f41
Lim-shweety <smile70200@naver.com>
2024-02-04 17:30:46 +0900

    Update PageTitle.tsx : update margin-top

--- src/app/_components/PageTitle.tsx
+++ src/app/_components/PageTitle.tsx
@@ -5,7 +5,7 @@ interface Props{
 }
 const PageTitle = ({children}:Props) => {
     return (
-        <div className="flex flex-row">
+        <div className="flex flex-row mt-[95px] ">
             <p className="text-[130px] mt-[10px] ml-[120px]">{children}</p>
             <img className="h-[60px] ml-[-15px] mt-[10px]" src="/Titlelogo.png" alt="Titlelogo"/>
         </div>
