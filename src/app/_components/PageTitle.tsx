7618e650af647867d390ba8b1c51b70c432b19be
Lim-shweety <smile70200@naver.com>
2024-02-05 17:19:44 +0900

    Update PageTitle.tsx : modify title left margin

--- src/app/_components/PageTitle.tsx
+++ src/app/_components/PageTitle.tsx
@@ -6,7 +6,7 @@ interface Props{
 const PageTitle = ({children}:Props) => {
     return (
         <div className="flex flex-row mt-[95px] ">
-            <p className="text-[130px] mt-[10px] ml-[120px]">{children}</p>
+            <p className="text-[100px] mt-[10px] ml-[200px]">{children}</p>
             <img className="h-[60px] ml-[-15px] mt-[10px]" src="/Titlelogo.png" alt="Titlelogo"/>
         </div>
     );
