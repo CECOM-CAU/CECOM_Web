d885176a93736574b4e0c9d8392e560c10db95a4
Lim-shweety <smile70200@naver.com>
2024-02-09 16:24:54 +0900

    Update NavBar : change zindex and logo margin

--- src/app/_components/NavBar/NavBar.tsx
+++ src/app/_components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import NavBarDesktop from "@/app/_components/NavBar/NavBarDesktop";
 
 export const NavBar = () => {
     return (
-        <div className="w-full flex flex-row fixed top-0">
+        <div className="w-full flex flex-row fixed top-0 z-20">
             <NavBarDesktop/>
         </div>
     )
