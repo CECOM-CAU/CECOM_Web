9f96c6e141dc09ed4faafd67c39da24c28cd78d5
yymin1022 <yymin1022@gmail.com>
2024-02-10 03:25:21 +0900

    Update FirebaseUtil : Added Member DB Functions

--- src/utils/FirebaseUtil.ts
+++ src/utils/FirebaseUtil.ts
@@ -1,9 +1,10 @@
 import {FirebaseApp, initializeApp} from "@firebase/app";
-import {Firestore, getFirestore} from "@firebase/firestore";
+import {doc, Firestore, getDoc, getFirestore} from "@firebase/firestore";
 import dotenv from "dotenv";
+import {AdminItem, Member} from "@/utils/Interfaces";
 
-let firebaseApp: FirebaseApp
-let firestoreDB: Firestore
+let firebaseApp: FirebaseApp | null = null;
+let firestoreDB: Firestore | null = null;
 
 dotenv.config();
 const firebaseConfig = {
@@ -14,6 +15,34 @@ const firebaseConfig = {
 };
 
 export const initFirebase = () => {
-    firebaseApp = initializeApp(firebaseConfig);
-    firestoreDB = getFirestore(firebaseApp);
+    if(firebaseApp == null || firestoreDB == null){
+        firebaseApp = initializeApp(firebaseConfig);
+        firestoreDB = getFirestore(firebaseApp);
+    }
+}
+
+const getMemberData = async (memberID: string)  => {
+    const memberDoc = await getDoc(doc(firestoreDB!, "Members", memberID));
+    const memberData: Member = {
+        comment: memberDoc.get("comment"),
+        department: memberDoc.get("department"),
+        github: memberDoc.get("github"),
+        id: memberDoc.id,
+        instagram: memberDoc.get("instagram"),
+        name: memberDoc.get("name"),
+        project: memberDoc.get("project")
+    };
+    return memberData;
+}
+
+export const getAdminList = async (year: number) => {
+    const adminDoc = await getDoc(doc(firestoreDB!, "Admin", year.toString()));
+    const adminList: Array<AdminItem> = [];
+    for (const doc of adminDoc.get("list")){
+        adminList.push({
+            member: await getMemberData(doc.member),
+            role: doc.role
+        });
+    }
+    return adminList;
 }
