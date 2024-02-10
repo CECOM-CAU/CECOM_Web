56f1e4711cf27335623be3fa8f216e23d694f9eb
Lim-shweety <smile70200@naver.com>
2024-02-10 17:14:12 +0900

    Update Activity : front and back card is changed to activityCard

--- src/app/activities/_components/ActivityCardBack.tsx
+++ src/app/activities/_components/ActivityCardBack.tsx
@@ -1,13 +1,6 @@
 import {ActivityItem} from "@/utils/Interfaces";
 
-interface Props {
-    content: string;
-    member: string[];
-    mentor: string;
-    title: string;
-}
-
-const ActivityCardBack = ({content, member, mentor, title}: Props) => {
+const ActivityCardBack = ({content, member, mentor, tag, thumbnail, title, id}: ActivityItem) => {
     const members: string = member.map((m) => m).join(', ')
     return (
         <div className="flex flex-col items-start max-w-[270px] h-[415px] bg-white rounded-lg shadow-lg mt-[15px] py-[40px] px-[20px]">
@@ -15,7 +8,7 @@ const ActivityCardBack = ({content, member, mentor, title}: Props) => {
             <div className="flex flex-row items-center mt-[10px]">
                 <div className="text-[15px] font-gmarket-m">{mentor}</div>
                 <div className="mx-[10px] font-gmarket-m">|</div>
-                <div className="text-[13px] font-gmarket-l"> {members}</div>
+                <div className="text-[13px] font-gmarket-l"> {member}</div>
             </div>
             <div className="text-[12px] mt-[10px] truncate text-pretty ">{content}</div>
             <div></div>
