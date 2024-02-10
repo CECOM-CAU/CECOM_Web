02f96b71309e90b275d2bef2320c0553fd42245d
yymin1022 <yymin1022@gmail.com>
2024-02-10 21:53:02 +0900

    Update getActivitiesList : Added Basic Function Call

--- src/app/api/activities/getActivitiesList/route.ts
+++ src/app/api/activities/getActivitiesList/route.ts
@@ -1,12 +1,22 @@
 import {NextRequest, NextResponse} from "next/server";
 import {API_RESULT} from "@/utils/Interfaces";
+import {getActivityList} from "@/utils/FirebaseUtil";
 
 export async function GET(_: NextRequest) {
     const apiResult: API_RESULT = {
         RESULT_CODE: 200,
-        RESULT_MSG: "Activities List API",
+        RESULT_MSG: "Success",
         RESULT_DATA: undefined
     }
 
-    return NextResponse.json(apiResult, { status: 200 })
+    const activityList = await getActivityList();
+    if(activityList === undefined || activityList.count === 0){
+        apiResult.RESULT_CODE = 100;
+        apiResult.RESULT_MSG = "An Error Occurred";
+        apiResult.RESULT_DATA = undefined;
+        return NextResponse.json(apiResult, { status: 200 });
+    }
+
+    apiResult.RESULT_DATA = activityList;
+    return NextResponse.json(apiResult, { status: 200 });
 }
