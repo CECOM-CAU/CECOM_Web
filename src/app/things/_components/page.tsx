eb296b2e1cad66e0c759d8bccca78b997458c5a5
sereene <salad1223@naver.com>
2024-02-10 18:54:05 +0900

    Update things : Use Dummy Data

--- src/app/things/_components/page.tsx
+++ src/app/things/_components/page.tsx
@@ -1,9 +1,22 @@
-const SmallBox = () => {
+import {ThingItem} from "@/utils/Interfaces";
+
+const SmallBox = ({description, id, name, photo, tag}:ThingItem) => {
     return (
         <div className={'border rounded-xl border-gray-400 flex flex-row flex-grow-0 hover:shadow-xl'}>
             <img className={'h-[100px] m-[20px]'} src="/Thingsphoto.jpg" alt="Things"/>
             <div className={'mt-[20px] mr-[30px] mb-[20px]'}>
-                <TextBox></TextBox>
+                <div className={'flex flex-col'}>
+                    <div className={'w-[200px] grid grid-cols-3 grid-rows-1 '}>
+                        <span className={'text-[24px] col-span-2 font-gmarket-m'}>{name}</span>
+                        <div className={'h-[20px] w-[55px] flex bg-cover rounded-2xl border-0 bg-gray-300'}>
+                            <span className={'ml-[3px] text-[12px] place-self-center font-gmarket'}>{tag}</span>
+                        </div>
+                    </div>
+                    <div className={'mb-[10px]'}>
+                        <hr className={'w-[200px] border-gray-500'}/>
+                    </div>
+                    <span className={'text-wrap w-[180px] text-[15px] font-gmarket'}>{description}</span>
+                </div>
             </div>
         </div>
     );
