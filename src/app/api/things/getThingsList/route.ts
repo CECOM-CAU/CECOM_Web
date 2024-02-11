8aca2b7ca9015a9dfcfc39b8f510ba971eb080c9
yymin1022 <yymin1022@gmail.com>
2024-02-12 05:00:42 +0900

    Update getThingList API : Added Basic Function Call Logic

--- src/app/api/things/getThingsList/route.ts
+++ src/app/api/things/getThingsList/route.ts
@@ -1,12 +1,22 @@
 import {NextRequest, NextResponse} from "next/server";
 import {API_RESULT} from "@/utils/Interfaces";
+import {getThingList} from "@/utils/FirebaseUtil";
 
 export async function GET(_: NextRequest) {
     const apiResult: API_RESULT = {
         RESULT_CODE: 200,
-        RESULT_MSG: "Things List API",
+        RESULT_MSG: "Success",
         RESULT_DATA: undefined
     }
 
+    const thingList = await getThingList();
+    if(thingList === undefined){
+        apiResult.RESULT_CODE = 100;
+        apiResult.RESULT_MSG = "An Error Occurred";
+        apiResult.RESULT_DATA = undefined;
+        return NextResponse.json(apiResult, { status: 200 });
+    }
+
+    apiResult.RESULT_DATA = thingList;
     return NextResponse.json(apiResult, { status: 200 })
 }
