b6683a8a2afef4227b20021ef50b14cb2f37d153
yymin1022 <yymin1022@gmail.com>
2024-02-13 01:04:56 +0900

    Update AddOnServer Project : Added Express Basic Codes

--- AddOnServer/src/index.ts
+++ AddOnServer/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+import express, { Request, Response } from "express";
+
+import dotenv from "dotenv";
+import http from "http";
+
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, World!");
+});
+
+server.listen(3001, "0.0.0.0", () => {
+    console.log("Server listen on port 3001");
+});
