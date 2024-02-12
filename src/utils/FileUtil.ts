938a35aa467281152f94624b5c5349451774f577
yymin1022 <yymin1022@gmail.com>
2024-02-13 04:41:13 +0900

    Update FileUtil : Added Exception for Add-On Server

--- src/utils/FileUtil.ts
+++ src/utils/FileUtil.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const getProjectThumbnail = async (projectID: string) => {
-    const res = await axios.get(`http://127.0.0.1:3001/getImage/${projectID}/thumb.png`);
-    return res.data;
+    try{
+        const res = await axios.get(`http://127.0.0.1:3001/getImage/${projectID}/thumb.png`);
+        return res.data;
+    }catch(e){
+        return undefined;
+    }
 }
