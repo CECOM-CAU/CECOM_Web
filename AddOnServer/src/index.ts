93f471a76c8df0fca011cf2ac97ff27188adb41a
yymin1022 <yymin1022@gmail.com>
2024-02-13 01:45:15 +0900

    Update AddOnServer Project : Added Image Loading Function

--- AddOnServer/src/index.ts
+++ AddOnServer/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 
 import dotenv from "dotenv";
 import http from "http";
+import * as fs from "fs";
 
 dotenv.config();
 const app = express();
@@ -9,13 +10,21 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
-app.get("/getImage", (req: Request, res: Response) => {
-    res.send("Get Image");
+app.get("/getImage/:projectID/:fileName", (req: Request, res: Response) => {
+    const params = req.params;
+    const filePath = `${process.env.FILE_DIR}/project/${params.projectID}/${params.fileName}`;
+    let fileBuffer: Buffer | undefined;
+    try{
+        fileBuffer = fs.readFileSync(filePath);
+        res.send(Buffer.from(fileBuffer!).toString("base64"));
+    }catch(e){
+        res.sendStatus(500);
+    }
 });
 
 app.get("/getPost", (req: Request, res: Response) => {
     res.send("Get Post");
-})
+});
 
 server.listen(3001, "0.0.0.0", () => {
     console.log("Server listen on port 3001");
