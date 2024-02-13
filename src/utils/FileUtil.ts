b1e4782f392858cc15f9470c97303e23d152010d
yymin1022 <yymin1022@gmail.com>
2024-02-14 04:36:26 +0900

    Update FileUtil : Applied Runtime Environmental Variable

--- src/utils/FileUtil.ts
+++ src/utils/FileUtil.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import {setDefaultResultOrder} from "dns";
+import {unstable_noStore} from "next/cache";
 
 export const getProjectThumbnail = async (projectID: string) => {
+    unstable_noStore();
     try{
         setDefaultResultOrder("ipv4first");
         const res = await axios.get(`http://${process.env.LOCALHOST_IP}:3001/getImage/${projectID}/thumb.png`);
