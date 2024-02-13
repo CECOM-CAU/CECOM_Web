762f1bf57a5ed7f887abdaf02162b1f3cf9e20e0
yymin1022 <yymin1022@gmail.com>
2024-02-14 04:36:14 +0900

    Update FileUtil : Applied LOCALHOST_IP Environmental Variable

--- src/utils/FileUtil.ts
+++ src/utils/FileUtil.ts
@@ -4,7 +4,7 @@ import {setDefaultResultOrder} from "dns";
 export const getProjectThumbnail = async (projectID: string) => {
     try{
         setDefaultResultOrder("ipv4first");
-        const res = await axios.get(`http://localhost:3001/getImage/${projectID}/thumb.png`);
+        const res = await axios.get(`http://${process.env.LOCALHOST_IP}:3001/getImage/${projectID}/thumb.png`);
         console.log(res);
         return res.data;
     }catch(e){
