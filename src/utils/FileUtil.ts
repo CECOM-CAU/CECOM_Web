1ea048e501641311ce1dcfc108b78b4042c204b8
yymin1022 <yymin1022@gmail.com>
2024-02-14 04:36:44 +0900

    Update FileUtil : Removed Debug Print

--- src/utils/FileUtil.ts
+++ src/utils/FileUtil.ts
@@ -7,10 +7,8 @@ export const getProjectThumbnail = async (projectID: string) => {
     try{
         setDefaultResultOrder("ipv4first");
         const res = await axios.get(`http://${process.env.LOCALHOST_IP}:3001/getImage/${projectID}/thumb.png`);
-        console.log(res);
         return res.data;
     }catch(e){
-        console.log(e);
         return undefined;
     }
 }
