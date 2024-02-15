628c9adcdef6a9823bdc7fefd882a6af5e9df082
yymin1022 <yymin1022@gmail.com>
2024-02-16 03:42:51 +0900

    Update FileUtil : Added Activity Photo Function

--- src/utils/FileUtil.ts
+++ src/utils/FileUtil.ts
@@ -2,6 +2,17 @@ import axios from "axios";
 import {setDefaultResultOrder} from "dns";
 import {unstable_noStore} from "next/cache";
 
+export const getActivityPhoto = async (activityID: string, photoIdx: number) => {
+    unstable_noStore();
+    try{
+        setDefaultResultOrder("ipv4first");
+        const res = await axios.get(`http://${process.env.LOCALHOST_IP}:3001/getImage/${activityID}/${photoIdx}.png`);
+        return res.data;
+    }catch(e){
+        return undefined;
+    }
+}
+
 export const getActivityThumbnail = async (projectID: string) => {
     unstable_noStore();
     try{
