d9f6a2d4f506a37973423d6cd7e6c65978f1a4a8
yymin1022 <yymin1022@gmail.com>
2024-02-16 04:22:40 +0900

    Update getMainImages : Added File Util Function Call

--- src/app/api/main/getMainImages/route.ts
+++ src/app/api/main/getMainImages/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
-import {API_RESULT} from "@/utils/Interfaces";
+import {API_RESULT, MainImage} from "@/utils/Interfaces";
 import {corsHeader} from "@/utils/CorsUtil";
+import {getMainImage} from "@/utils/FileUtil";
 
 export async function GET(_: NextRequest) {
     const apiResult: API_RESULT = {
@@ -9,5 +10,23 @@ export async function GET(_: NextRequest) {
         RESULT_DATA: undefined
     }
 
+    const mainImageData: MainImage = {
+        event: await getMainImage("event"),
+        mentoring: await getMainImage("mentoring"),
+        project: await getMainImage("project"),
+        study: await getMainImage("study")
+    }
+
+    if(mainImageData.event === undefined
+        || mainImageData.mentoring === undefined
+        || mainImageData.project === undefined
+        || mainImageData.study === undefined){
+        apiResult.RESULT_CODE = 100;
+        apiResult.RESULT_MSG = "An Error Occurred";
+        apiResult.RESULT_DATA = undefined;
+        return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
+    }
+
+    apiResult.RESULT_DATA = mainImageData;
     return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
 }
