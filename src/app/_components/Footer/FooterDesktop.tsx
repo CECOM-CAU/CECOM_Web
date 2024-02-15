fb2f7aa39fa9f91ddb59020ad32e661b6b5c4d88
yymin1022 <yymin1022@gmail.com>
2024-02-16 04:29:00 +0900

    Update FooterDesktop : Applied Link for Instagram / GitHub

--- src/app/_components/Footer/FooterDesktop.tsx
+++ src/app/_components/Footer/FooterDesktop.tsx
@@ -1,13 +1,17 @@
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub, faInstagram} from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 const FooterDesktop = () => {
     return (
         <div className="w-full flex flex-col justify-center items-center h-[300px] mt-[100px]">
             <div className="flex flex-row justify-center mb-[30px]">
-
-                <FontAwesomeIcon icon={faInstagram} size="2x" className="mr-[20px]" />
-                <FontAwesomeIcon icon={faGithub} size="2x" className="mr-[20px]" />
+                <Link href={"https://instagram.com/cecom_cau"}>
+                    <FontAwesomeIcon icon={faInstagram} size="2x" className="mr-[20px]" />
+                </Link>
+                <Link href={"https://github.com/CECOM-CAU"}>
+                    <FontAwesomeIcon icon={faGithub} size="2x" className="mr-[20px]" />
+                </Link>
             </div>
             <div className="flex flex-row items-end">
                 <div className="font-gmarket-b text-[20px]">Contact</div><div className="font-gmarket-m text-[20px] ml-[10px]">cecom.cau@gmail.com</div>
