6ce3d926aa7c6ead6c982672706ea1e18bdcf29e
yymin1022 <yymin1022@gmail.com>
2024-02-16 03:33:13 +0900

    Update getActivitiesData : Added Firebase Function Call

--- src/app/api/activities/getActivitiesData/[id]/route.ts
+++ src/app/api/activities/getActivitiesData/[id]/route.ts
@@ -1,13 +1,22 @@
 import {NextRequest, NextResponse} from "next/server";
 import {API_RESULT} from "@/utils/Interfaces";
 import {corsHeader} from "@/utils/CorsUtil";
+import {getActivityData} from "@/utils/FirebaseUtil";
 
 export async function GET(_: NextRequest, {params}: {params: {id: string}}) {
     const apiResult: API_RESULT = {
         RESULT_CODE: 200,
-        RESULT_MSG: `Activities Data API / ID is ${params.id}`,
+        RESULT_MSG: "Success",
         RESULT_DATA: undefined
     }
 
-    return NextResponse.json(apiResult, { status: 200, headers: corsHeader })
+    const activityData = await getActivityData(params.id);
+    if(activityData === undefined){
+        apiResult.RESULT_CODE = 100;
+        apiResult.RESULT_MSG = "An Error Occurred";
+        apiResult.RESULT_DATA = undefined;
+        return NextResponse.json(apiResult, {status: 200, headers: corsHeader});
+    }
+
+    return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
 }
