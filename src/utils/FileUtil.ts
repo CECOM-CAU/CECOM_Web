a0a8d8b1af1dc0e06406568b3873f55d44b14e48
yymin1022 <yymin1022@gmail.com>
2024-02-16 03:43:06 +0900

    Update FileUtil : Fixed Typo on getActivityThumbnail

--- src/utils/FileUtil.ts
+++ src/utils/FileUtil.ts
@@ -13,11 +13,11 @@ export const getActivityPhoto = async (activityID: string, photoIdx: number) => 
     }
 }
 
-export const getActivityThumbnail = async (projectID: string) => {
+export const getActivityThumbnail = async (activityID: string) => {
     unstable_noStore();
     try{
         setDefaultResultOrder("ipv4first");
-        const res = await axios.get(`http://${process.env.LOCALHOST_IP}:3001/getImage/${projectID}/thumb.png`);
+        const res = await axios.get(`http://${process.env.LOCALHOST_IP}:3001/getImage/${activityID}/thumb.png`);
         return res.data;
     }catch(e){
         return undefined;
