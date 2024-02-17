a60ee4ed2afb1bd942c05568b0666f27137da98e
Lim-shweety <smile70200@naver.com>
2024-02-17 18:23:06 +0900

    Added MobileWhatWeDo : add What We Do components

---
+++ src/app/_components/mainPage/mobile/MobileWhatLeft.tsx
@@ -0,0 +1,21 @@
+interface Props{
+    children:React.ReactNode;
+    comment:string[];
+    thumbnail:string;
+}
+const MobileWhatLeft= ({children,comment,thumbnail}:Props) => {
+    return (
+        <div className="flex flex-col w-full mb-[30px]">
+            <img src={thumbnail} alt="thumbnail" className="flex justify-end rounded-[60px] w-[330px] h-[280px]"/>
+            <div className="flex w-[330px] border-b-[1px] border-[#000000] mt-[15px] mb-[5px]"></div>
+            <div className="flex flex-col justify-start items-start w-full">
+                <div className='font-gmarket-m text-[30px] px-[10px] pb-[5px]'>{children}</div>
+                {comment.map((comment:string)=>(
+                    <div className="text-start text-pretty w-full font-gmarket-l text-[15px] px-[10px]">{comment}</div> ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MobileWhatLeft;
---
+++ src/app/_components/mainPage/mobile/MobileWhatRight.tsx
@@ -0,0 +1,21 @@
+interface Props{
+    children:React.ReactNode;
+    comment:string[];
+    thumbnail:string;
+}
+const MobileWhatRight= ({children,comment,thumbnail}:Props) => {
+    return (
+        <div className="flex flex-col w-full mb-[30px]">
+            <img src={thumbnail} alt="thumbnail" className="flex justify-end rounded-[60px] w-[330px] h-[280px]"/>
+            <div className="flex border-b-[1px] border-[#000000] mt-[15px] mb-[5px]"></div>
+            <div className="flex flex-col justify-end items-start w-full">
+                <div className='flex w-full justify-end font-gmarket-m text-[30px] px-[10px] pb-[5px]'>{children}</div>
+                {comment.map((comment:string)=>(
+                    <div className="flex w-full justify-end text-start text-pretty font-gmarket-l text-[15px] px-[10px]">{comment}</div> ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MobileWhatRight;
