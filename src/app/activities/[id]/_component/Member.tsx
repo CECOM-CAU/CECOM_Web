da0c72006cab99c9209acad26b31c085bd537ddd
Lim-shweety <smile70200@naver.com>
2024-02-19 20:33:04 +0900

    Update Member.tsx : add mobile version style

--- src/app/activities/[id]/_component/Member.tsx
+++ src/app/activities/[id]/_component/Member.tsx
@@ -10,9 +10,8 @@ const Member = ({icon, name, url}: ActivityContentRoleItemMember) => {
         <div className="mx-[5px]" onMouseOver={() => setIsHover(true)} onMouseOut={() => setIsHover(false)}>
             {isHover ?  <Bubble>{name}</Bubble>: <></>}
             <Link href={url}>
-                <img className="rounded-full w-[25px] h-[25px]"
-                     src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLk6U3b0bu-HwA2BtU-hub0tg8msPeUCQeEA&usqp=CAU"}/>
-
+                <img className="rounded-full w-[20px] h-[20px] lg:w-[25px] lg:h-[25px]"
+                     src={`data:image/png;base64,${icon}`}/>
             </Link>
         </div>
     );
