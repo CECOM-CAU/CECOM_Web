671928480c3a9bb93261dd14efd8b3d66c3c000e
Lim-shweety <smile70200@naver.com>
2024-02-21 16:44:42 +0900

    Update MobileHome.tsx : connect data using axios

--- src/app/_components/mainPage/mobile/MobileHome.tsx
+++ src/app/_components/mainPage/mobile/MobileHome.tsx
@@ -2,19 +2,10 @@
 import MobileMainLogo from "@/app/_components/mainPage/mobile/MobileMainLogo";
 import MobileWho from "@/app/_components/mainPage/mobile/MobileWho";
 import MobileWhatWeDo from "@/app/_components/mainPage/mobile/MobileWhatWeDo";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 export const MobileHome = () => {
-    useEffect(() => {
-        axios.get("https://cecom.dev/api/main/getMainImages")
-            .then((res) => {
-                console.log(res);
-            }).catch((err) => {
-            console.log(err);
-        });
-    },[]);
-
     return (
         <div className="block lg:hidden z-0">
             <div className="h-[100vh] w-full bg-gradient-to-br from-blue-100 to-transparent via-transparent">
