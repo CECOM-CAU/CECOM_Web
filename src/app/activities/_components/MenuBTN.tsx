d3bf909508221e768732a1914d2c955a32a0457c
Lim-shweety <smile70200@naver.com>
2024-02-22 20:02:21 +0900

    Fix activities components : remove useless code line

--- src/app/activities/_components/MenuBTN.tsx
+++ src/app/activities/_components/MenuBTN.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-interface props{
+
+interface props {
     children: React.ReactNode;
     isClicked: boolean;
 
 }
-const MenuBTN = ({children, isClicked}:props) => {
+
+const MenuBTN = ({children, isClicked}: props) => {
     const NAV_BTN_STYLE_CLICKED = "text-[18px] rounded-3xl mx-[10px] font-gmarket-m lg:text-xl lg:mx-[15px]"
     const NAV_BTN_STYLE_NORMAL = "text-[18px] flex justify-center mx-[10px] lg:text-xl lg:mx-[15px]";
     return (
-
-        <div className={isClicked?NAV_BTN_STYLE_CLICKED:NAV_BTN_STYLE_NORMAL}>
-            {children}</div>
-
+        <div className={isClicked ? NAV_BTN_STYLE_CLICKED : NAV_BTN_STYLE_NORMAL}>
+            {children}
+        </div>
     );
 
 }
