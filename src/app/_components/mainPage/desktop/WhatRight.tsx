c43ef12b3454b67a774efc23c0b10bc9bd6865d4
Lim-shweety <smile70200@naver.com>
2024-02-23 03:42:46 +0900

    Update MainPage desktop WhatLeft and Right : add img object-cover

--- src/app/_components/mainPage/desktop/WhatRight.tsx
+++ src/app/_components/mainPage/desktop/WhatRight.tsx
@@ -6,14 +6,13 @@ interface Props{
 const WhatRight= ({children,comment,thumbnail}:Props) => {
     return (
         <div className="flex flex-row w-full">
-
             <div className="flex flex-col justify-center items-center w-[700px]">
                 <div className='font-gmarket-m text-[40px] px-[20px] pb-[10px] border-b-[1px] border-[#000000] mb-[20px]'>{children}</div>
                 {comment.map((comment:string,index:number)=>(
                     <div className="text-center text-pretty w-[600px] font-gmarket-l text-[20px] px-[20px]" key={index}>{comment}</div> ))
                 }
             </div>
-            <img src={thumbnail} alt="thumbnail" className="flex justify-end rounded-[60px] w-[450px] h-[350px]"/>
+            <img src={thumbnail} alt="thumbnail" className="flex justify-end rounded-[60px] w-[450px] h-[350px] object-cover"/>
         </div>
     )
 }
