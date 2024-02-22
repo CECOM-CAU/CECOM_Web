0dee88253342a692c2693ef50dd457b703de3847
Lim-shweety <smile70200@naver.com>
2024-02-23 03:40:08 +0900

    Update MobileWhat left and right : add img resize-cover

--- src/app/_components/mainPage/mobile/MobileWhatLeft.tsx
+++ src/app/_components/mainPage/mobile/MobileWhatLeft.tsx
@@ -6,7 +6,7 @@ interface Props{
 const MobileWhatLeft= ({children,comment,thumbnail}:Props) => {
     return (
         <div className="flex flex-col w-full mb-[30px]">
-            <img src={thumbnail} alt="thumbnail" className="flex justify-end rounded-[60px] w-[330px] h-[280px]"/>
+            <img src={thumbnail} alt="thumbnail" className="flex justify-end rounded-[60px] w-[330px] h-[280px] object-cover"/>
             <div className="flex w-[330px] border-b-[1px] border-[#000000] mt-[15px] mb-[5px]"></div>
             <div className="flex flex-col justify-start items-start w-full">
                 <div className='font-gmarket-m text-[30px] px-[10px] pb-[5px]'>{children}</div>
--- src/app/_components/mainPage/mobile/MobileWhatRight.tsx
+++ src/app/_components/mainPage/mobile/MobileWhatRight.tsx
@@ -6,7 +6,7 @@ interface Props{
 const MobileWhatRight= ({children,comment,thumbnail}:Props) => {
     return (
         <div className="flex flex-col w-full mb-[30px]">
-            <img src={thumbnail} alt="thumbnail" className="flex justify-end rounded-[60px] w-[330px] h-[280px]"/>
+            <img src={thumbnail} alt="thumbnail" className="flex justify-end rounded-[60px] w-[330px] h-[280px] object-cover"/>
             <div className="flex border-b-[1px] border-[#000000] mt-[15px] mb-[5px]"></div>
             <div className="flex flex-col justify-end items-start w-full">
                 <div className='flex w-full justify-end font-gmarket-m text-[30px] px-[10px] pb-[5px]'>{children}</div>
