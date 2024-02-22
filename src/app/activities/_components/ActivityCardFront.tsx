064d647b3c7d189703497642ab8127755d23d5c2
Lim-shweety <smile70200@naver.com>
2024-02-23 03:49:48 +0900

    Update ActivityCardFront.tsx : add img object-cover property

--- src/app/activities/_components/ActivityCardFront.tsx
+++ src/app/activities/_components/ActivityCardFront.tsx
@@ -5,7 +5,7 @@ const ActivityCardFront = ({member, mentor, tag, thumbnail, title}: ActivityItem
     return (
         <div
             className="flex flex-col items-center max-w-[270px] h-[430px] bg-white rounded-lg shadow-lg mt-[25px]">
-            <img className="w-[240px] min-h-[240px] rounded-lg mx-[15px] my-[15px]" src={thumbnail} alt={title}/>
+            <img className="w-[240px] min-h-[240px] rounded-lg mx-[15px] my-[15px] object-cover" src={thumbnail} alt={title}/>
             <div className="flex flex-col w-full h-48 px-[15px]">
                 <div className="flex flex-row flex-wrap justify-start mt-[10px]">
                     <span className="font-gmarket-b ">{title}&nbsp;&nbsp;</span>
