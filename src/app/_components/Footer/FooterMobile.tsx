2cbf3cdd84c3a0d899e693be6ef5a77129b40ea7
yymin1022 <yymin1022@gmail.com>
2024-02-23 01:29:13 +0900

    Update Footer : Adjust Text Alignment of FooterMobile

--- src/app/_components/Footer/FooterMobile.tsx
+++ src/app/_components/Footer/FooterMobile.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const FooterMobile = () => {
     return (
         <div className="w-[340px] flex flex-col justify-center items-center h-[300px] mt-[100px]">
-            <div className="flex flex-row justify-center mb-[30px]">
+            <div className="w-full flex flex-row justify-center mb-[30px]">
                 <Link href={"https://instagram.com/cecom_cau"}>
                     <FontAwesomeIcon icon={faInstagram} size="2x" className="mr-[20px]" />
                 </Link>
@@ -13,14 +13,19 @@ const FooterMobile = () => {
                     <FontAwesomeIcon icon={faGithub} size="2x" className="mr-[20px]" />
                 </Link>
             </div>
-            <div className="flex flex-row items-end">
-                <div className="font-gmarket-b text-[20px]">Contact</div><div className="font-gmarket-m text-[20px] ml-[10px]">cecom.cau@gmail.com</div>
+            <div className="w-full flex flex-row justify-center">
+                <div className="font-gmarket-b text-[20px]">
+                    Contact
+                </div>
+                <div className="font-gmarket-m text-[20px] ml-[10px]">
+                    cecom.cau@gmail.com
+                </div>
             </div>
-            <div className="border-[1px] border-[#000000] w-[200px] my-[20px]"></div>
-            <div className="text-[15px] my-[10px]">
+            <div className="w-[200px] border-[1px] border-[#000000] my-[20px]"></div>
+            <div className="w-full text-[15px] text-center my-[10px]">
                 Designed by Yubin Seo / Developed by Yongmin Yoo, Sooghyun Lim, Serin Jeong
             </div>
-            <div className="text-[15px] mb-[10px]">
+            <div className="w-full text-[15px] text-center mb-[10px]">
                 &#169; 2024 CECOM ALL RIGHT RESERVED.
             </div>
         </div>
