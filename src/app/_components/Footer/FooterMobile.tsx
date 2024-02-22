8a075ecab3ae6597e086fa22a11e87617d16bab2
yymin1022 <yymin1022@gmail.com>
2024-02-23 01:51:37 +0900

    Update Footer : Adjust Layout of FooterMobile

--- src/app/_components/Footer/FooterMobile.tsx
+++ src/app/_components/Footer/FooterMobile.tsx
@@ -5,27 +5,30 @@ import Link from "next/link";
 const FooterMobile = () => {
     return (
         <div className="w-[340px] flex flex-col justify-center items-center h-[300px] mt-[100px]">
-            <div className="w-full flex flex-row justify-center mb-[30px]">
+            <div className="w-full flex flex-row justify-center mb-[15px]">
                 <Link href={"https://instagram.com/cecom_cau"}>
-                    <FontAwesomeIcon icon={faInstagram} size="2x" className="mr-[20px]" />
+                    <FontAwesomeIcon icon={faInstagram} size="2x" className="mr-[15px]" />
                 </Link>
                 <Link href={"https://github.com/CECOM-CAU"}>
-                    <FontAwesomeIcon icon={faGithub} size="2x" className="mr-[20px]" />
+                    <FontAwesomeIcon icon={faGithub} size="2x" className="ml-[15px]" />
                 </Link>
             </div>
             <div className="w-full flex flex-row justify-center">
-                <div className="font-gmarket-b text-[20px]">
+                <div className="font-gmarket-b text-[15px]">
                     Contact
                 </div>
-                <div className="font-gmarket-m text-[20px] ml-[10px]">
+                <div className="font-gmarket-m text-[15px] ml-[10px]">
                     cecom.cau@gmail.com
                 </div>
             </div>
             <div className="w-[200px] border-[1px] border-[#000000] my-[20px]"></div>
-            <div className="w-full text-[15px] text-center my-[10px]">
-                Designed by Yubin Seo / Developed by Yongmin Yoo, Sooghyun Lim, Serin Jeong
+            <div className="w-full text-[13px] text-center">
+                Designed by Yubin Seo
             </div>
-            <div className="w-full text-[15px] text-center mb-[10px]">
+            <div className="w-full text-[13px] text-center mb-[10px]">
+                Developed by Yongmin Yoo<br/>Sooghyun Lim, Serin Jeong
+            </div>
+            <div className="w-full text-[13px] text-center mt-[10px] mb-[-100px]">
                 &#169; 2024 CECOM ALL RIGHT RESERVED.
             </div>
         </div>
