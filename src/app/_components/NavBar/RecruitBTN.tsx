89eb1fa6015340d4f6a000f3ab01429082c77514
yymin1022 <yymin1022@gmail.com>
2024-02-29 18:55:52 +0900

    Update NavBar : Added Google Form URL for RecruitBTN

--- src/app/_components/NavBar/RecruitBTN.tsx
+++ src/app/_components/NavBar/RecruitBTN.tsx
@@ -9,7 +9,9 @@ const RecruitBTN = ({children}: Props) => {
     const NAV_BTN_STYLE_NORMAL = "px-4 py-2 mr-[20px] ml-[20px] bg-primary-cecom_blue text-white rounded-3xl font-gmarket-m";
 
     return (
-        <Link href={""}>
+        <Link href={
+            "https://docs.google.com/forms/d/e/1FAIpQLSdX_hqjckEER3C0kOIa70wN8OFtk9GnojKILWJ6r0oeOuDSWQ/viewform"
+        }>
             <div className={NAV_BTN_STYLE_NORMAL}>{children}</div>
         </Link>
     )
