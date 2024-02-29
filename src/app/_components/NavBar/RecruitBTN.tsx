d0d559441153f5c37ac6659ca72a5b1b4baeee7d
yymin1022 <yymin1022@gmail.com>
2024-02-29 18:55:34 +0900

    Update NavBar : Removed Link Props of RecruitBTN

--- src/app/_components/NavBar/RecruitBTN.tsx
+++ src/app/_components/NavBar/RecruitBTN.tsx
@@ -3,14 +3,13 @@ import Link from "next/link";
 
 interface Props {
     children: React.ReactNode;
-    link: string;
 }
 
-const RecruitBTN = ({children, link}: Props) => {
+const RecruitBTN = ({children}: Props) => {
     const NAV_BTN_STYLE_NORMAL = "px-4 py-2 mr-[20px] ml-[20px] bg-primary-cecom_blue text-white rounded-3xl font-gmarket-m";
 
     return (
-        <Link href={link}>
+        <Link href={""}>
             <div className={NAV_BTN_STYLE_NORMAL}>{children}</div>
         </Link>
     )
