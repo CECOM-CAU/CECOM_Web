60fee5178060d74748a0e19c9ca9dabe8980389d
yymin1022 <yymin1022@gmail.com>
2024-08-02 00:34:19 +0900

    Update RecruitSubmissionDetail API : Added Slug Parameter for ID

--- src/app/api/recruit/getSubmissionDetail/[id]/route.ts
+++ src/app/api/recruit/getSubmissionDetail/[id]/route.ts
@@ -3,7 +3,7 @@ import {API_RESULT} from "@/utils/Interfaces";
 import {corsHeader} from "@/utils/CorsUtil";
 import {getRecruitSubmissionDetail} from "@/utils/FirebaseUtil";
 
-export async function GET(_: NextRequest) {
+export async function GET(_: NextRequest, {params}: {params: {id: string}}) {
     const apiResult: API_RESULT = {
         RESULT_CODE: 200,
         RESULT_MSG: "Success",
