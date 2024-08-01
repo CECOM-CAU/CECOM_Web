fbb040eee686fa6d152d19940489750abde92d13
yymin1022 <yymin1022@gmail.com>
2024-08-02 01:05:13 +0900

    Update Recruit Submission Post PAI : Added Post Submission Function Call

--- src/app/api/recruit/postSubmission/route.ts
+++ src/app/api/recruit/postSubmission/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import {API_RESULT, RecruitSubmissionDetail} from "@/utils/Interfaces";
 import {corsHeader} from "@/utils/CorsUtil";
+import {postRecruitingSubmission} from "@/utils/FirebaseUtil";
 
 export async function POST(req: NextRequest) {
     const apiResult: API_RESULT = {
@@ -15,6 +16,13 @@ export async function POST(req: NextRequest) {
         id: submissionBody.id,
         name: submissionBody.name,
         timestamp: 0
+    }
+
+    const submissionResult = await postRecruitingSubmission(submissionData);
+    if(!submissionResult){
+        apiResult.RESULT_CODE = 100;
+        apiResult.RESULT_MSG = "An Error Occurred";
+        apiResult.RESULT_DATA = undefined;
     }
 
     return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
