cc1a9c74c3a195f2199c5ca2f1540fa1ca083cb7
yymin1022 <yymin1022@gmail.com>
2024-08-02 00:23:07 +0900

    Update RecruitSubmissionList API : Added Function Call

--- src/app/api/recruit/getSubmissionList/route.ts
+++ src/app/api/recruit/getSubmissionList/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import {API_RESULT} from "@/utils/Interfaces";
 import {corsHeader} from "@/utils/CorsUtil";
+import {getAdminList, getRecruitSubmissionList} from "@/utils/FirebaseUtil";
 
 export async function GET(_: NextRequest) {
     const apiResult: API_RESULT = {
@@ -8,6 +9,16 @@ export async function GET(_: NextRequest) {
         RESULT_MSG: "Success",
         RESULT_DATA: undefined
     }
+
+    const submissionList = await getRecruitSubmissionList();
+    if(submissionList === undefined || submissionList.data.length == 0){
+        apiResult.RESULT_CODE = 100;
+        apiResult.RESULT_MSG = "An Error Occurred";
+        apiResult.RESULT_DATA = undefined;
+        return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
+    }
+
+    apiResult.RESULT_DATA = submissionList;
 
     return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
 }
