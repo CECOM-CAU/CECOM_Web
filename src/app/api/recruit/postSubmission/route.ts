43c8f380104ab5021110df26513e482d9cd02270
yymin1022 <yymin1022@gmail.com>
2024-08-02 01:01:47 +0900

    Update Recruit Submission Post API : Added JSON Data Parse Function

--- src/app/api/recruit/postSubmission/route.ts
+++ src/app/api/recruit/postSubmission/route.ts
@@ -1,12 +1,20 @@
 import {NextRequest, NextResponse} from "next/server";
-import {API_RESULT} from "@/utils/Interfaces";
+import {API_RESULT, RecruitSubmissionDetail} from "@/utils/Interfaces";
 import {corsHeader} from "@/utils/CorsUtil";
 
-export async function POST(_: NextRequest) {
+export async function POST(req: NextRequest) {
     const apiResult: API_RESULT = {
         RESULT_CODE: 200,
         RESULT_MSG: "Success",
         RESULT_DATA: undefined
+    }
+
+    const submissionBody: RecruitSubmissionDetail = JSON.parse(await req.text());
+    const submissionData: RecruitSubmissionDetail = {
+        department: submissionBody.department,
+        id: submissionBody.id,
+        name: submissionBody.name,
+        timestamp: 0
     }
 
     return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
