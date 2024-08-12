576e800a485490274b360cde5160c250f9acf93f
Lim-shweety <smile70200@naver.com>
2024-08-12 19:13:45 +0900

    Update MobilePageTitle.tsx : added left padding at title

--- src/app/_components/MobilePageTitle.tsx
+++ src/app/_components/MobilePageTitle.tsx
@@ -4,7 +4,7 @@ export const MobilePageTitle = ({children}: { children: React.ReactNode }) => {
     return (
         <div className="flex flex-col items-center lg:hidden">
             <div className="flex justify-center mt-[80px]">
-                <p className="text-[50px]">{children}</p>
+                <p className="text-[50px] pl-[15px]">{children}</p>
                 <img className="h-[30px] ml-[-5px] mt-[-5px]" src="/Titlelogo.png" alt="Titlelogo"/>
             </div>
             <div className="w-[150px] border-[#000000] border-y-[1px] mt-[10px] mb-[20px]"></div>
