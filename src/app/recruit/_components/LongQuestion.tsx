84248cb41d977cb9a8183a78445aa6052f7c581f
Lim-shweety <smile70200@naver.com>
2024-08-21 01:48:50 +0900

    Added LongQuestion.tsx : added long Question component

---
+++ src/app/recruit/_components/LongQuestion.tsx
@@ -0,0 +1,20 @@
+interface Props {
+    children: string;
+    length: number;
+}
+
+const LongQuestion = ({children, length}: Props) => {
+    return (
+        <div className="flex flex-col items-start w-full m-[10px] pb-[10px] border-b-[1px] border-[#000000]">
+            {children}
+                <div className="flex w-full justify-end items-end">
+                    {length}/500자<img className='flex w-[30px] h-[30px] ml-[10px]' src={'pen.png'}/>
+                </div>
+
+
+        </div>
+    )
+};
+
+
+export default LongQuestion;
