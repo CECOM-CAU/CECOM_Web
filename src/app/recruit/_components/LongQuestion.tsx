096bcd76ea113c00ec41782bcdc71544fefb1e4c
Lim-shweety <smile70200@naver.com>
2024-08-25 00:25:46 +0900

    Update LongQuestion.tsx : add font size

--- src/app/recruit/_components/LongQuestion.tsx
+++ src/app/recruit/_components/LongQuestion.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const LongQuestion = ({children, length}: Props) => {
     return (
-        <div className="flex flex-col items-start w-full m-[10px] pb-[10px] border-b-[1px] border-[#000000]">
+        <div className="flex flex-col items-start w-full mb-[10px] text-[18px] pb-[10px] border-b-[1px] border-[#000000]">
             {children}
                 <div className="flex w-full justify-end items-end">
                     {length}/500자<img className='flex w-[30px] h-[30px] ml-[10px]' src={'pen.png'}/>
