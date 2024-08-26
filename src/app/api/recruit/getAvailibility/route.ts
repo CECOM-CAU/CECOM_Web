3cbf1eb9d385e9945a2efa1137ba50d907a1dc8f
yymin1022 <yymin1022@gmail.com>
2024-08-26 11:41:41 +0900

    Update RecruitAvailibility API : Added Firebase Call

--- src/app/api/recruit/getAvailibility/route.ts
+++ src/app/api/recruit/getAvailibility/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import {API_RESULT} from "@/utils/Interfaces";
 import {corsHeader} from "@/utils/CorsUtil";
+import {getRecruitAvailibility} from "@/utils/FirebaseUtil";
 
 export async function GET(_: NextRequest) {
     const apiResult: API_RESULT = {
@@ -8,6 +9,9 @@ export async function GET(_: NextRequest) {
         RESULT_MSG: "Success",
         RESULT_DATA: undefined
     }
+
+    const recruitAvail = await getRecruitAvailibility();
+    apiResult.RESULT_DATA = recruitAvail;
 
     return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
 }
