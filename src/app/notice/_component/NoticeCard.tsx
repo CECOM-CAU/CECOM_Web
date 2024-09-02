13db995e988a2cd5d39f83bd3c2e0c2f5f051ca0
Lim-shweety <smile70200@naver.com>
2024-09-02 14:24:25 +0900

    Update NoticeCard.tsx : modify img size

--- src/app/notice/_component/NoticeCard.tsx
+++ src/app/notice/_component/NoticeCard.tsx
@@ -2,7 +2,7 @@ import {id} from "postcss-selector-parser";
 import Link from "next/link";
 
 interface Props {
-    id: number;
+    id: string;
     img: string;
     title: string;
     tag?: string;
@@ -17,14 +17,14 @@ const NoticeCard = ({img, id, title, tag, date}: Props) => {
             className="flex flex-col w-[165px] lg:w-[300px] items-start mb-[18px] lg:mb-[40px] lg:mx-[20px]">
             <Link href={`/notice/${id}`}>
                 <div className="w-[165px] h-[165px] lg:w-[300px] lg:h-[150px] rounded-md">
-                    <img className='object-cover w-[165px] h-[165px] lg:w-[300px] lg:h-[150px] rounded-md shadow-lg' src={img}/>
+                    <img className='object-cover w-[165px] h-[165px] lg:w-[300px] lg:h-[150px] rounded-md shadow-lg' src={`data:image/png;base64,${img}`}/>
                 </div>
                 <div className='font-gmarket-m text-[15px] lg:text-[16px] mt-[10px] break-words w-[165px] lg:w-[300px]'>{title}</div>
                 <div className='flex flex-row'>
                     <div
-                        className="flex text-[10px] h-[20px] pt-[1px] text-center lg:text-[12px] align-bottom justify-center items-center bg-primary-cecom_blue_light rounded-3xl px-[6px] mr-[10px] mt-[1px]">{tag}</div>
+                        className="flex text-[10px] h-[20px] pt-[3px] text-center lg:text-[12px] align-bottom justify-center items-center bg-primary-cecom_blue_light rounded-3xl px-[6px] mr-[5px] mt-[1px]">{tag}</div>
                     <div
-                        className="flex text-[10px] h-[20px] pt-[1px] text-center lg:text-[12px] align-bottom justify-center items-center bg-primary-lightgray30 rounded-3xl px-[6px] mr-[10px] mt-[1px]">{date}</div>
+                        className="flex text-[10px] h-[20px] pt-[3px] text-center lg:text-[12px] align-bottom justify-center items-center bg-primary-lightgray30 rounded-3xl px-[6px] mr-[5px] mt-[1px]">{date}</div>
                 </div>
             </Link>
         </div>
