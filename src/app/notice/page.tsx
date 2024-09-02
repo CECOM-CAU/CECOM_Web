72efdf8ad47f449e3aa019c5aa6767a91bebdb12
Lim-shweety <smile70200@naver.com>
2024-09-02 14:25:49 +0900

    Update notice\page.tsx : add axios

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,10 +1,22 @@
+'use client';
 import PageTitle from "@/app/_components/PageTitle";
 import {MobilePageTitle} from "@/app/_components/MobilePageTitle";
-import React from "react";
+import React, {useEffect} from "react";
 import NoticeCard from "@/app/notice/_component/NoticeCard";
+import axios from "axios";
+import {NoticeList, NoticeListItem} from "@/utils/Interfaces";
 
 
 const NoticePage = () => {
+    const [noticeList, setNoticeList] = React.useState<NoticeListItem[]>([]);
+    useEffect(() => {
+        axios.get(`/api/notice/getNoticeList`)
+            .then((res) => {
+                    setNoticeList(res.data.RESULT_DATA.data);
+                }
+            )
+    }, []);
+
     return (
         <div>
             <div className={'hidden lg:block'}>
@@ -15,21 +27,11 @@ const NoticePage = () => {
             </div>
             <div className='flex flex-row w-full justify-center '>
                 <div className='flex flex-col w-full lg:w-[1020px] items-center '>
-                    <div className='flex flex-row w-full px-[15px] justify-evenly lg:w-[1050px] lg:justify-start flex-wrap'>
-                        <NoticeCard img={'/MemberFemale.png'} id={1} title={"룰루랄랄 쿠쿠루삐뽕"} tag={"#홍보부"}
-                                    date={"8/19-9/20"}/>
-                        <NoticeCard img={'/MemberMale.png'} id={2} title={"룰루랄랄 쿠쿠루삐뽕"} tag={"#홍보부"}
-                                    date={"8/19-9/20"}/>
-                        <NoticeCard img={'/MemberFemale.png'} id={3} title={"룰루랄랄 쿠쿠루삐뽕"} tag={"#홍보부"}
-                                    date={"8/19-9/20"}/>
-                        <NoticeCard img={'/MemberFemale.png'} id={1} title={"룰루랄랄 쿠쿠루삐뽕"} tag={"#홍보부"}
-                                    date={"8/19-9/20"}/>
-                        <NoticeCard img={'/MemberMale.png'} id={2} title={"룰루랄랄 쿠쿠루삐ddddfdf fgdfgg df fddfdfddfddfsdsdfd뽕"} tag={"#홍보부"}
-                                    date={"8/19-9/20"}/>
-                        <NoticeCard img={'/MemberFemale.png'} id={3} title={"룰루랄랄 쿠쿠루삐뽕"} tag={"#홍보부"}
-                                    date={"8/19-9/20"}/>
-                        <NoticeCard img={'/MemberFemale.png'} id={3} title={"룰루랄랄 쿠쿠루삐뽕"} tag={"#홍보부"}
-                                    date={"8/19-9/20"}/>
+                    <div
+                        className='flex flex-row w-full px-[6px] justify-evenly lg:w-[1050px] lg:justify-start flex-wrap'>
+                        {noticeList.map((notice: NoticeListItem) => (
+                            <NoticeCard img={notice.thumbnail} id={notice.id} title={notice.title} tag={notice.part}
+                                        date={notice.date}/>))}
                         <div className='w-[160px] lg:w-[340px]'></div>
                     </div>
                 </div>
