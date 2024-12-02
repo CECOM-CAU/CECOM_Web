c8ba9006cb7b966b7aea3a8d2595b2dfc7a86505
Lim-shweety <smile70200@naver.com>
2024-12-02 17:57:47 +0900

    Update DesktopMainLogo.tsx : Set width property

--- src/app/_components/mainPage/desktop/DesktopMainLogo.tsx
+++ src/app/_components/mainPage/desktop/DesktopMainLogo.tsx
@@ -1,14 +1,14 @@
 const DesktopMainLogo = () => {
     const thin = "flex text-[80px] h-[100px] mr-[20px]";
-    const bold = "flex text-[80px] h-[100px] font-gmarket-m";
+    const bold = "flex text-[80px] w-full h-[100px] font-gmarket-m";
     return (
-        <div className="flex flex-col justify-center x-[962px] h-[100vh]">
-            <div className="flex flex-row">
-                <div className={thin}>That's</div>
+        <div className="flex flex-col justify-center w-[962px] min-h-[100vh]">
+            <div className="flex flex-row w-full">
+                <div className={`${thin} w-auto`}>That's</div>
                 <div className={bold}>crazy idea,</div>
             </div>
-            <div className="flex flex-row">
-                <div className={thin}>let's</div>
+            <div className="flex flex-row w-full">
+                <div className={`${thin} w-auto`}>let's</div>
                 <div className={bold}>do &#8213;&#8213;&#8213; that</div>
             </div>
 
