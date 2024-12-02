f44d94bcb413f03e81ff78ff38a02283dc638799
Lim-shweety <smile70200@naver.com>
2024-12-02 17:45:46 +0900

    Update MobileWhatWeDo.tsx : modified props passing method due to changes in parent props

--- src/app/_components/mainPage/mobile/MobileWhatWeDo.tsx
+++ src/app/_components/mainPage/mobile/MobileWhatWeDo.tsx
@@ -13,18 +13,18 @@ const MobileWhatWeDo = ({project,study,mentoring,event} :MainImage) => {
             </div>
             <div className="flex flex-col w-[340px]">
                 <MobileWhatLeft
-                    thumbnail={`data:image/png;base64,${mentoring}`}
+                    thumbnail={mentoring??""}
                     comment={["해당 분야에 대해 경험과 지식이 있는", "멘토가 6주 동안 멘티를 지도하는 활동입니다."]}>Mentoring</MobileWhatLeft>
 
                 <MobileWhatRight comment={["프로젝트 혹은 그에 준하는 경험이 있는", "부원들이 모여 재미있는 아이디어를 나누고", "이를 실현시키는 활동입니다."]}
-                                 thumbnail={`data:image/png;base64,${project}`}>Project</MobileWhatRight>
+                                 thumbnail={project??""}>Project</MobileWhatRight>
 
                 <MobileWhatLeft
-                    thumbnail={`data:image/png;base64,${study}`}
+                    thumbnail={study??""}
                     comment={["해당 분야에 관심이 있는 부원들이 모여", "공부하고, 배운 것을 나누는 활동입니다."]}>Study Group</MobileWhatLeft>
 
                 <MobileWhatRight comment={["재미있는 아이디어가 자유롭게 오갈 수 있는", "환경을 만들기 위해","부원들 사이의 친목을 도모할 수 있는","다양한 친목 활동을 진행합니다."]}
-                                 thumbnail={`data:image/png;base64,${event}`}>Events</MobileWhatRight>
+                                 thumbnail={event??""}>Events</MobileWhatRight>
 
             </div>
         </div>
